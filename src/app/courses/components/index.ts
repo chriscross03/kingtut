5e2b0c6ee68de129e8e62cc949c4e9fa26fef610
ericliudx <ericdongxuliu@gmail.com>
2025-10-27 16:46:53 -0400

    made components for courses page

---
+++ src/app/courses/components/index.ts
@@ -0,0 +1,4 @@
+export { default as CourseItem } from "./CourseItem";
+export { default as CourseList } from "./CourseList";
+export { default as CourseLoading } from "./CourseLoading";
+export { default as CourseError } from "./CourseError";
