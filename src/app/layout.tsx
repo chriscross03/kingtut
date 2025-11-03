11ac5a0ef1caa0e307e745c29ab86f83a065c2d4
Chris Kim <chrischansu@gmail.com>
2025-11-03 13:59:19 -0500

    displaying welcome back

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import AuthStatus from "../components/AuthStatus";
+import Providers from "../components/Providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Providers>
+          <header>
+            <AuthStatus />
+          </header>
+          {children}
+        </Providers>
       </body>
     </html>
   );
